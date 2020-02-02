7fc35b68eeaf7b9a6f78b9abaabf41814d5da073
Arcobaleno <137518538@qq.com>
2020-02-02 16:56:33 +0800

    first commit

---
+++ Assets/scripts/key_lv1_1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key_lv1_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D ray_switch = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 1f), new Vector2(transform.position.x, transform.position.y + 150f), 1.5f);
+        if(ray_switch.collider!=null&& (ray_switch.collider.tag == "p1" || ray_switch.collider.tag == "p2"))
+        {
+            glob.gate_1 = 1;
+        }
+        else
+        {
+            glob.gate_1 = 0;
+        }
+    }
+}
